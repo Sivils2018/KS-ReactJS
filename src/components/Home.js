2479eb710522f5d0caa18970814d8a620a5a966f
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2022-03-27 10:44:20 -0400

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"; 
+import Card from 'Cards/Cards'; 
+
+
+
+// creating a react-based js file
+export default function Home (){
+    return (
+        <><div>
+            <h3 style={{padding: "50px", display: "flex", justifyContent: "center"}}>Categories</h3>
+        </div><div>
+            <Card>
+                <ul>
+                     <li><Link to="/Products"> Sports </Link></li> 
+                        <li> <Link to="/Products"> Beauty </Link></li> 
+                     <li> <Link to= "/Products"> Clothes </Link></li>
+                </ul>
+            </Card>
+            </div></> 
+        ); 
+    }
---
+++ src/components/Wishlist.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// creating a react-based js file
+export default class Wishlist extends Component {
+    render() {
+        return (
+                <div>
+ {/* testing code for the page functionality: */}
+ <p>Welcome to Wishlist Component!</p>
+ </div>
+         );
+    }
+}
