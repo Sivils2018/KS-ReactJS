dd5ef0a5416931a841fe4937975695e7a2305aa2
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2022-03-27 10:48:49 -0400

    Add files via upload

---
+++ src/components/Register.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import '../components/App.css'; 
+import axios from "axios"; 
+
+export default class Register extends Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName= this.onChangeLastName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this); 
+        this.onSubmit = this.onSubmit.bind(this); 
+
+        this.state = 
+        {   
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: ''
+
+        }
+    }
+
+         onChangeFirstName(e) 
+        {
+            this.setState({first_name: e.target.value}); 
+        }
+
+        onChangeLastName(e)
+        {
+            this.setState({last_name: e.target.value}); 
+        }
+
+        onChangeEmail(e)
+        {
+            this.setState({email: e.target.value}); 
+        }
+        onChangePassword(e)
+        {
+            this.setState({password: e.target.value}); 
+        }
+
+        onSubmit(e)
+        {
+            e.preventDefault(); 
+
+            const obj = 
+            {
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                email: this.state.email,
+                password: this.state.password
+               
+            }; 
+            
+            axios 
+            .post("http://localhost/insert.php", obj, 
+            {
+                header: 
+                {
+                    'content-type': 'multipart/form-data'
+                }
+            })
+            .then(response => 
+            {    
+            console.log(response.data);
+            this.setState({
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: ''
+            });
+        }); 
+        }
+
+
+    render() {
+        return (
+            <div className="register">
+            <h3>Register</h3>
+            <form onSubmit ={this.onSubmit} >
+             <div className="form-group">
+            <label>First Name:</label>
+            <input
+            type="text"
+            className="form-control"
+            value={this.state.first_name}
+            onChange = {this.onChangeFirstName}
+            />
+            </div>
+            <div className="form-group">
+            <label>Last Name:</label>
+            <input
+            type="text"
+            className="form-control"
+            value={this.state.last_name}
+            onChange={this.onChangeLastName}
+            />
+            </div>
+            <div className="form-group">
+            <label>Email:</label>
+            <input
+            type="text"
+            className="form-control"
+            value={this.state.email}
+            onChange={this.onChangeEmail}
+            />
+            </div>
+            <div className="form-group">
+            <label>Password:</label>
+            <input 
+            type="password"
+            className="form-control"
+            value={this.state.password}
+            onChange={this.onChangePassword}
+            />
+            </div>
+            <div className="form-group">
+            <input
+            type="submit"
+            value="Register User"
+            className="btn btn-primary"
+            />
+            </div>
+            </form>
+            </div>
+        )
+    }
+}
