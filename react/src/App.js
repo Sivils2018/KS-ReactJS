f7d41e910145c45735f682c9f09ba7e2bc8eb465
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2022-04-07 20:58:42 -0400

    Add files via upload

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Link, useParams, useHistory, useNavigate} from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, Nav, NavDropdown} from "react-router-dom"; 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import {Login, Wishlist, Register, Home, Products} from './components';
+import {Login, Wishlist, Register, Home, Products, Header} from './components';
 import { Navbar, Button, Badge} from "reactstrap";
+import React, { Component } from "react";
+import authenticated from "constants/authenticated";
 
-
-
-
-
-function App()
+class App extends Component {
+constructor(props)
 {
+      super(props);
+      this.state = ({
+            updated: '',
+            redirect: false
+      });
+      this.update = this.update.bind(this);
+}
 
+update() {
+      this.setState({updated: true})
+}
+
+onClick(e){
+      e.preventDefault();
+      localStorage.clear("name");
+}
+
+render () 
+{
 return (
-  <Router>
+<Router>
  <div className="container">
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <Link to={"/Home"} className="navbar-brand" style={{color: "red", fontSize: "40px", fontFamily: "serif", borderStyle: "solid"}}> IWantThis </Link>
@@ -20,11 +37,20 @@ return (
             <li className="nav-item"> <Link to={"/Home"} className="nav-link">Home </Link> </li>
             <li className="nav-item"> <Link to={"/Login"} className="nav-link">Login </Link> </li>
             <li className="nav-item"> <Link to={"/Register"} className="nav-link"> Register </Link> </li>
-            <li className="nav-item"> <Link to={"/Wishlist"} className="nav-link"> Wishlist <Badge bg= "primary"> 0 </Badge></Link></li>
-            <button> </button>
       </ul>
-
 </div>
+      <div>
+            <nav>
+            <div>
+            <ul className = "navbar-nav mr-auto" style= {{margin: "20px"}}>
+                <li className = "nav-item"> Welcome! { localStorage.getItem("name") ? `${localStorage.getItem("name")}` : " " } </li>
+                <li className="nav-item"> <Link to={"/Wishlist"} className="nav-link"> <Badge bg= "primary"> 0 </Badge></Link></li>
+            {authenticated.getAuthentication() ? <button onClick={() => {this.setState({redirect: false}); authenticated.changeAuthentication(); localStorage.clear("name");}}> Log Out </button> :
+            <button onClick={() => {this.setState({redirect: true}); authenticated.changeAuthentication();}}> Log In </button> }
+            </ul> 
+            </div>
+            </nav>
+      </div>
 </nav>
  <Routes>
       <Route path="/" element={<Home />} /> 
@@ -36,7 +62,8 @@ return (
    </Routes> 
  </div> 
  </Router>
-)}
+);
+}}
 
 
 
