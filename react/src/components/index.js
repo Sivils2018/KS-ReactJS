1405c4f98bacba864d372c6c487ca26fb60d0ed7
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2022-04-16 16:19:25 -0400

    Add files via upload

---
+++ react/src/components/index.js
@@ -0,0 +1,14 @@
+import Login from './Login'; 
+import Register from './Register';
+import Wishlist from './Wishlist';
+import Home from './Home'; 
+import Products from './Products';
+import CategoriesList from "./CategoriesList";
+import ProductsList from './ProductsList';
+import SingleProduct from './SingleProduct';
+
+
+
+
+
+export {Login, Register, Wishlist, Home, Products, CategoriesList, SingleProduct, ProductsList}; 
