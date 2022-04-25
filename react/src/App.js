7b23b126238159b7337631538fb59ae4f66a3c26
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2022-04-24 22:04:24 -0400

    Add files via upload

--- react/src/App.js
+++ react/src/App.js
@@ -31,25 +31,6 @@ render ()
 return (
 <Router>
  <div className="container">
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-<Link to={"/Home"} className="navbar-brand" style={{color: "red", fontSize: "40px", fontFamily: "serif", borderStyle: "solid"}}> IWantThis </Link>
- <div className="collapse navbar-collapse" id="navbarSupportContent">
-      <ul className="navbar-nav mr-auto" style={{margin: "20px"}}>
-            <li className="nav-item"> <Link to={"/Home"} className="nav-link">Home </Link> </li>
-            {localStorage.getItem("name") ? '' : <>
-            <li className="nav-item"> <Link to={"/Login"} className="nav-link">Login </Link> </li>
-            <li className="nav-item"> <Link to={"/Register"} className="nav-link"> Register </Link> </li>
-            </>
-            }
-      </ul>
-</div>
-      <div>
-            <nav>
-            {!localStorage.getItem("name") ? (<><Link className="dropdown-item" to="/Login"> Log In </Link><Link className="dropdown-item" to="/Register"> Sign Up</Link></>):(<><Link className="dropdown-item" to="/Wishlist"> Welcome, {localStorage.getItem("name")} <Badge bg= "primary"> 0 </Badge></Link></>)}
-
-            </nav>
-      </div>
-</nav>
  <Routes>
       <Route path="/" element={<Home />} /> 
       <Route path="/Home" element={<Home />} /> 
@@ -60,7 +41,7 @@ return (
       <Route path='/Product/:id' element={<SingleProduct />} />  
    </Routes> 
  </div> 
- </Router>
+ </Router> 
 );
 }}
 
